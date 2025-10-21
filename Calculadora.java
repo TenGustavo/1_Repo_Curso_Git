fcc9874fd476e1b98b949440c352e44a24214d8a
TenGustavo <gustavo.luiz.ferreira.de.souza@ccc.ufcg.edu.br>
2025-10-21 20:44:09 -0300

    Criei o java calculadora e adicionei meu nome ao arquivo

---
+++ Calculadora.java
@@ -0,0 +1,11 @@
+public class Calculadora {
+    public static void main(String args[]){
+        int a = 5;
+        int b = 10;
+        System.out.println(soma(a, b));
+    }
+
+    public static int soma(int a, int b){
+        return a + b;
+    }
+}
